0e461be5d58d40ba2f296430ae370687961612a1
markv55 <janmarkvink@gmail.com>
2019-01-02 20:48:40 +0100

    tweeen krpano

--- backup/13dd30dec/index.js
+++ backup/13dd30dec/index.js
@@ -35,7 +35,6 @@ io.on('connection', function(socket) {
 
 	
 	// uitleg: onderstaand ontvangt eerste socket x, en daarna een 2e waarde. deze tweede waarde wordt opgeslagen in variable (x). vervolgens wordt deze variable als 3e socket meegestuurd
-	// onderstaande x en y ontvangt hij van unity script readCameraAndSendXYendXY.cs. dit script stuurt de x en y variable en deze index.js stuurt het 1 op 1 door!
     socket.on('x', function(x) {
 		console.log("The x is: " + x);
 		io.emit('message', 'x', x);
