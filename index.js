f91fd3d541881dba8675694f6069110c214afa2d
markv55 <janmarkvink@gmail.com>
2020-01-07 12:55:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -601,7 +601,6 @@ app.get('/ad_product_rotterdam', function (req, res) {
 	//KlikAanKlikUit(11, 260, 7, 5).off(1, 2);
 	
 	io.emit('message', 'pi', 'ad_product_rotterdam' );
-	io.emit('stuur', '1', '2', '3', '4' );
 	console.log ('wat staat hier');		
 	res.redirect('/');
 });
